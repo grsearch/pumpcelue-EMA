45e14cc6a73aae6555b9a4b9aabcbb0d34fd1061
grsearch <grsearch@gmail.com>
2026-03-22 16:17:06 +0800

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,10 @@ module.exports = {
   rsi: {
     period:    parseInt(process.env.RSI_PERIOD)      || 7,
     buyCross:  parseFloat(process.env.RSI_BUY_CROSS) || 30,  // 上穿买入
-    tpPct:     parseFloat(process.env.TP_PCT)        || 50,  // 止盈百分比
+    sellHigh:  parseFloat(process.env.RSI_SELL_HIGH) || 80,  // RSI > 80 卖出
+    sellCross: parseFloat(process.env.RSI_SELL_CROSS)|| 70,  // RSI 下穿 70 卖出
+    tpPct:        parseFloat(process.env.TP_PCT)           || 50,  // 止盈百分比
+    reAddDropPct: parseFloat(process.env.RE_ADD_DROP_PCT)     || 20,  // 再次加仓所需跌幅
     slPct:     parseFloat(process.env.SL_PCT)        || 50,  // 止损百分比
   },
 };
